327c017258b01e3f7c558c0960d2becfbb07712b
elkowar <5300871+elkowar@users.noreply.github.com>
2021-05-28 18:28:22 +0200

    some cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#![feature(iter_advance_by)]
 use bevy::{
     input::{keyboard::KeyboardInput, ElementState},
     prelude::*,
@@ -10,13 +9,14 @@ const ARBITRARY_OSU_TIME_OFFSET: i32 = 55;
 
 const HIT_ACCURACY: i64 = 100;
 
-const OSU_FILE: &str = "songs/aotd/DragonForce - Ashes of the Dawn (Nao Tomori) [Futsuu].osu";
-const OGG_FILE: &str = "songs/aotd/audio.ogg";
+// const OSU_FILE: &str = "songs/aotd/DragonForce - Ashes of the Dawn (Nao Tomori) [Futsuu].osu";
+// const OGG_FILE: &str = "songs/aotd/audio.ogg";
 // const OSU_FILE: &str = "songs/osu/Polyphia - O.D. (Melwoine) [Insane].osu";
 // const OGG_FILE: &str = "songs/od.ogg";
-// const OSU_FILE: &str = "songs/insight/Haywyre - Insight (Twiggykun) [Normal].osu";
-// const OGG_FILE: &str = "songs/insight/audio.ogg";
+const OSU_FILE: &str = "songs/insight/Haywyre - Insight (Twiggykun) [Normal].osu";
+const OGG_FILE: &str = "songs/insight/audio.ogg";
 
+#[allow(unused)]
 macro_rules! some_or_return {
     ($x:expr) => {
         match $x {
@@ -24,6 +24,12 @@ macro_rules! some_or_return {
             None => return,
         }
     };
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub enum GameState {
+    Loading,
+    Playing,
 }
 
 pub struct Block;
@@ -40,10 +46,12 @@ pub struct AudioChannels {
 }
 
 #[derive(Default)]
-pub struct SongStartedAt(Option<u128>);
+pub struct SongStartedAt(u128);
 
 #[derive(Default)]
-pub struct TimeInSong(Option<u128>);
+pub struct TimeInSong {
+    millis: u128,
+}
 
 pub struct SongStarted;
 
@@ -86,14 +94,14 @@ impl FromWorld for BongoMap {
                     } else {
                         actual_ms_per_beat = (-next_timing_point.ms_per_beat as f32 / 100.0) * non_negative_ms_per_beat;
                     };
-                    let _ = timing_points.advance_by(1);
+                    let _ = timing_points.next();
                 }
             }
             times.push(time);
             match object {
                 osuparse::HitObject::Slider(slider) => {
                     let slider_end = time + slider_length(slider.pixel_length, slider_mult, actual_ms_per_beat) as i32;
-                    // times.push(slider_end);
+                    times.push(slider_end);
                 }
                 osuparse::HitObject::Spinner(spinner) => times.push(spinner.end_time),
                 osuparse::HitObject::HoldNote(hold) => times.push(hold.end_time),
@@ -112,33 +120,26 @@ fn load_assets(asset_server: Res<AssetServer>) {
 
 fn handle_song_loading(
     mut commands: Commands,
+    mut game_state: ResMut<State<GameState>>,
     asset_server: Res<AssetServer>,
     audio_channels: Res<AudioChannels>,
     time: Res<Time>,
     audio: Res<Audio>,
     mut song_started_at: ResMut<SongStartedAt>,
 ) {
-    if song_started_at.0.is_some() {
-        return;
-    }
     let handle = asset_server.get_handle(OGG_FILE);
     match asset_server.get_load_state(&handle) {
-        bevy::asset::LoadState::NotLoaded => {}
         bevy::asset::LoadState::Loading => {
             commands.insert_resource(ClearColor(Color::rgb(0.1, 0.1, 0.1)));
         }
-        bevy::asset::LoadState::Failed => {}
         bevy::asset::LoadState::Loaded => {
             commands.insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)));
             audio.play_in_channel(handle, &audio_channels.music);
-            song_started_at.0 = Some(time.time_since_startup().as_millis());
-
-            let bong_handle = asset_server.load("sfx/bong.ogg");
-            audio.play_in_channel(bong_handle, &audio_channels.bong);
-
+            song_started_at.0 = time.time_since_startup().as_millis();
             dbg!(song_started_at.0);
-            // writer.send(SongStarted);
+            game_state.set(GameState::Playing).unwrap();
         }
+        _ => {}
     }
 }
 
@@ -154,7 +155,6 @@ fn setup_audio(mut commands: Commands, audio: Res<Audio>) {
     let music = AudioChannel::new("music".to_string());
     audio.set_volume_in_channel(0.1, &music);
     audio.set_volume_in_channel(0.3, &bong);
-
     commands.insert_resource(AudioChannels { bong, music });
 }
 
@@ -165,18 +165,16 @@ fn epic_block_time(
     mut bongo_map: ResMut<BongoMap>,
     time_in_song: Res<TimeInSong>,
 ) {
-    let since_start = some_or_return!(time_in_song.0);
-
     let mut count = 0;
     let texture_handle = asset_server.load("sprites/smol_square.png");
     let mat_handle = materials.add(texture_handle.into());
-    for time in bongo_map.data.iter().take_while(|x| **x as i64 - (since_start as i64) < 2000) {
+    for time in bongo_map.data.iter().take_while(|x| **x as i64 - (time_in_song.millis as i64) < 2000) {
         count += 1;
         commands
             .spawn_bundle(SpriteBundle {
                 material: mat_handle.clone(),
                 transform: Transform {
-                    translation: Vec3::new(*time as f32 - since_start as f32, 0.0, 0.0),
+                    translation: Vec3::new(*time as f32 - time_in_song.millis as f32, 0.0, 0.0),
                     ..Default::default()
                 },
                 ..Default::default()
@@ -190,9 +188,8 @@ fn epic_block_time(
 }
 
 fn kill_blocks(mut commands: Commands, time_in_song: Res<TimeInSong>, query: Query<(Entity, &Timed), With<Block>>) {
-    let since_start = some_or_return!(time_in_song.0);
     for (entity, timed) in query.iter() {
-        if (timed.0 as i64) - (since_start as i64) < -1000 {
+        if (timed.0 as i64) - (time_in_song.millis as i64) < -1000 {
             commands.entity(entity).despawn_recursive();
         }
     }
@@ -208,10 +205,9 @@ fn keyboard_events(
     query: Query<(Entity, &Timed)>,
 ) {
     for _ in key_evr.iter().filter(|x| x.state == ElementState::Pressed) {
-        let since_start = some_or_return!(time_in_song.0);
         let closest_hit = query
             .iter()
-            .map(|(entity, timed)| (entity, i64::abs(timed.0 as i64 - (since_start as i64))))
+            .map(|(entity, timed)| (entity, i64::abs(timed.0 as i64 - (time_in_song.millis as i64))))
             .min_by_key(|(_, diff)| *diff)
             .filter(|(_, diff)| *diff < HIT_ACCURACY);
         if let Some((entity, ..)) = closest_hit {
@@ -223,33 +219,36 @@ fn keyboard_events(
 }
 
 fn move_blocks(time_in_song: Res<TimeInSong>, mut query: Query<(&mut Transform, &Timed), With<Block>>) {
-    let since_start = some_or_return!(time_in_song.0);
     for (mut transform, timed) in query.iter_mut() {
-        transform.translation.x = timed.0 as f32 - since_start as f32;
+        transform.translation.x = timed.0 as f32 - time_in_song.millis as f32;
     }
 }
 
 fn update_time(time: ResMut<Time>, song_started_at: Res<SongStartedAt>, mut time_in_song: ResMut<TimeInSong>) {
-    let started_at = some_or_return!(song_started_at.0);
+    let started_at = song_started_at.0;
     let since_start = time.time_since_startup().as_millis() - started_at;
-    time_in_song.0 = Some(since_start);
+    time_in_song.millis = since_start;
 }
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_plugin(AudioPlugin)
+        .add_state(GameState::Loading)
         .init_resource::<BongoMap>()
         .init_resource::<SongStartedAt>()
         .init_resource::<TimeInSong>()
         .add_startup_system(setup.system())
         .add_startup_system(setup_audio.system())
         .add_startup_system(load_assets.system())
-        .add_system(epic_block_time.system())
-        .add_system(update_time.system())
-        .add_system(move_blocks.system())
-        .add_system(kill_blocks.system())
-        .add_system(keyboard_events.system())
-        .add_system(handle_song_loading.system())
+        .add_system_set(SystemSet::on_update(GameState::Loading).with_system(handle_song_loading.system()))
+        .add_system_set(
+            SystemSet::on_update(GameState::Playing)
+                .with_system(epic_block_time.system())
+                .with_system(update_time.system())
+                .with_system(move_blocks.system())
+                .with_system(kill_blocks.system())
+                .with_system(keyboard_events.system()),
+        )
         .run();
 }
